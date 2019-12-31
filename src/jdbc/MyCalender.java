9b26175bf7729853766b58496fa782af40caebfd
dolly-bagaria <48085890+dolly-bagaria@users.noreply.github.com>
2019-12-31 00:11:44 -0800

    Add files via upload

--- src/jdbc/MyCalender.java
+++ src/jdbc/MyCalender.java
@@ -3,6 +3,7 @@ package jdbc;
 import java.util.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 
 public class MyCalender {
@@ -23,12 +24,32 @@ public class MyCalender {
 		Calendar calndr=Calendar.getInstance();
 		calndr.setTime(dt);
 		calndr.setTimeInMillis(millis);
-		
+		/*
 		LocalDate Id=LocalDate.now();
 		System.out.println(Id);
 		DateTimeFormatter dtf=DateTimeFormatter.ofPattern("dd-MM-YYYY");
 		System.out.println(dtf.format(Id));
+		*/
+		//DaysToBirthday("19-10-2020");
+		DaysToBirthday("19-10-2020");
 
+	}
+	
+	static void DaysToBirthday(String s) {
+		LocalDate Id=LocalDate.now();
+		LocalDate today= Id;
+		System.out.println(today);
+		
+		DateTimeFormatter dtf=DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate futureDate=LocalDate.from(dtf.parse(s));
+		int year=futureDate.getYear();
+		int month=futureDate.getMonthValue();
+		int day=futureDate.getDayOfMonth();
+		LocalDate birth= LocalDate.of(year, month, day);
+		System.out.println(birth);
+		
+		long intervalDays = ChronoUnit.DAYS.between(today, birth);
+	    System.out.println("Total number of days between dates:" + intervalDays);
 	}
 
 }
